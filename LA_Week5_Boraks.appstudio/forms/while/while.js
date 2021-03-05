3b1791e338c3bd8e572807181bf80746db404707
ellieb1234 <ellieboraks@creighton.edu>
2021-03-05 09:53:40 -0600

    Update while.js

--- LA_Week5_Boraks.appstudio/forms/while/while.js
+++ LA_Week5_Boraks.appstudio/forms/while/while.js
@@ -1,10 +1,6 @@
-/*
-
-*/
-
 let userInput=prompt("How many times do you want to loop?")
 let counter=1
 while (counter<=userInput) {
-  console.log('This is loop iteration'+counter+'.')
+  console.log('This is loop iteration '+counter+'.')
   counter++
 }
