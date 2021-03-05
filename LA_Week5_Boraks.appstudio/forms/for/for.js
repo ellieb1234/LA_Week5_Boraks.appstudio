eb3da2e3344d652c3a876faa30a63091eab1bb34
ellieb1234 <ellieboraks@creighton.edu>
2021-03-05 09:53:38 -0600

    Update for.js

--- LA_Week5_Boraks.appstudio/forms/for/for.js
+++ LA_Week5_Boraks.appstudio/forms/for/for.js
@@ -1,9 +1,9 @@
 /*
 outputs the names from an array to the console, one name per line using a for loop to cycle through the array. 
-
+*/
 
 let names=['Bob','Janet','Tom','Erin','Lucy','Alyssa','Dustin']
 
 for (let i=0; i<names.length;i++)
   console.log(names[i])
-  */
+  
