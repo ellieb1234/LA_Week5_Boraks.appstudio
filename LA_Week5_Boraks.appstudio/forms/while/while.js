b70cef93cc2fadd2fe10c475f119124ec13a4ce1
ellieb1234 <ellieboraks@creighton.edu>
2021-03-05 09:43:06 -0600

    Create while.js

---
+++ LA_Week5_Boraks.appstudio/forms/while/while.js
@@ -0,0 +1,10 @@
+/*
+
+*/
+
+let userInput=prompt("How many times do you want to loop?")
+let counter=1
+while (counter<=userInput) {
+  console.log('This is loop iteration'+counter+'.')
+  counter++
+}
