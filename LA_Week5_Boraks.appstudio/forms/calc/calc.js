1b9775b46361931889fecbefe573adc63005af5b
ellieb1234 <ellieboraks@creighton.edu>
2021-03-05 09:53:36 -0600

    Create calc.js

---
+++ LA_Week5_Boraks.appstudio/forms/calc/calc.js
@@ -0,0 +1,7 @@
+let num1=prompt("enter a number")
+let num2=prompt("enter another number")
+function Calculate(num1, num2){
+  return num1+num2
+}
+let newSum=Calculate(num1,num2)
+console.log(`The sum of ${num1} and ${num2} is ${newSum}`)
